24fcb6d3357a9c66a1693614d5644111b9ca8cc6
Robert Taylor <nano77nm@gmail.com>
2019-10-13 01:25:21 -0600

    Delete test.R

--- test.R
+++
@@ -1,5 +0,0 @@
-titanic <- read.csv('/Users/rmtaylor/ML/bootcamp/Titanic/datasciencedojo-capstone/train.csv', 
-                    header=TRUE, sep=',')
-head(titanic)
-dim(titanic)
-summary(titanic$Age)
